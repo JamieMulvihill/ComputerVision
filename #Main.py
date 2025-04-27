5b9b43c392f2edf8f76bce49060b43e80b861c45
JamieMulvihill <44208828+JamieMulvihill@users.noreply.github.com>
2025-04-27 10:30:21 +0100

    added conlutional layer and maxpooling

--- #Main.py
+++ #Main.py
@@ -22,11 +22,21 @@ print(training_labels.shape)
 print(test_images.shape)
 print(test_labels.shape)
 
+NumPixelX = 28
+NumPixelY = 28
+NumColorChannels = 1
+training_images = training_images.reshape(60000, NumPixelX, NumPixelY, NumColorChannels)
 training_images = training_images/255
+test_images = test_images.reshape(10000, NumPixelX, NumPixelY, 1)
 test_images = test_images/255
 
 model = Sequential([
-    layers.Flatten(input_shape=(28,28)),
+    layers.Conv2D(64, (3,3), activation='relu',
+      input_shape=(NumPixelX, NumPixelY, NumColorChannels)),
+    layers.MaxPooling2D(2,2),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+    layers.Flatten(),
     layers.Dense(128, activation=tf.nn.relu),
     layers.Dense(10, activation=tf.nn.softmax)
 ])
